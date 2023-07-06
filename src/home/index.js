fd73eab2df3bf84d0d31652a20e8f7b342f7236d
Steve Karuma <s.karuma@huqas.org>
2023-07-06 05:27:45 +0300

    Added homepage sections

--- src/home/index.js
+++ src/home/index.js
@@ -1,11 +1,20 @@
 import Header from "components/header";
 import Footer from "components/footer";
+import Hero from "./hero";
+import Specials from "./specials";
+import Testimonials from "./testimonials";
+import About from "./about";
 
 const Home = () => {
 	return (
 		<>
 			<Header />
-			<main>homepage</main>
+			<main>
+				<Hero />
+				<Specials />
+				<Testimonials />
+				<About />
+			</main>
 			<Footer />
 		</>
 	);
